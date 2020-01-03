9b1b3dbcadadf7c1098c73c21ee43363421cbb46
SanskritiL <32401372+SanskritiL@users.noreply.github.com>
2020-01-03 11:06:00 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,452 @@
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Onlysndp : Home</title>
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/icon" href="assets/images/favicon.ico" />
+	<!-- Font Awesome -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Slick slider -->
+	<link href="assets/css/slick.css" rel="stylesheet">
+	<!-- Gallery Lightbox -->
+	<link href="assets/css/magnific-popup.css" rel="stylesheet">
+	<!-- Theme color -->
+	<link id="switcher" href="assets/css/theme-color/default-theme.css" rel="stylesheet">
+
+	<!-- Main Style -->
+
+	<link href="./assets/css/swiper.min.css" type="text/css" rel="stylesheet">
+	<link href="style.css" type="text/css" rel="stylesheet">
+	<!-- Fonts -->
+
+	<!-- Open Sans for body font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
+	<!-- Poppins for Title -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+	<!-- Start Header -->
+	<header id="mu-header" class="" role="banner">
+		<div class="container">
+			<nav class="navbar navbar-default mu-navbar">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+
+						<!-- Text Logo -->
+						<a class="navbar-brand" href="#"></a>
+
+						<!-- Image Logo -->
+						<!-- <a class="navbar-brand" href="#"><img src="assets/images/logo.png"></a> -->
+
+
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav mu-menu navbar-right">
+							<li><a href="#">HOME</a></li>
+							<li><a href="#mu-about">ABOUT ME</a></li>
+							<li><a href="#mu-service">SERVICES</a></li>
+							<li><a href="#mu-portfolio">PORTFOLIO</a></li>
+							<li><a href="#mu-contact">CONTACT</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</div>
+	</header>
+	<!-- End Header -->
+
+
+	<!-- Start Featured Slider -->
+
+
+
+	<!-- Start Featured Slider -->
+
+	<section id="mu-featured-slider">
+		<div id="particles-js"></div>
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-md-12">
+					<div class="mu-featured-area">
+						<h1>Sandeep Khanal</h1>
+
+						<!-- Start text slide -->
+						<div class="mu-featured-slider-wrapper">
+							<div class="mu-featured-slider" id="typed-strings">
+								<p>I am <span>a Photographer</span></p>
+								<p>I am <span>a Video Producer</span></p>
+								<p>I am <span>a Motion Designer</span></p>
+							</div>
+							<span id="typed"></span>
+						</div>
+						<!-- Start text slide -->
+
+						<div class="mu-featured-content">
+							<p>I believe the film and storytelling have the power to change people's opinion for good. Films offer a medium of storytelling and visual entertainment which gives pleasure and meaning to people. It’s the best way to express the ideas and stories that needed to be heard. </p>
+							<a href="#mu-portfolio" class="mu-primary-btn view-my-work-btn">VIEW MY WORK</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Start Featured Slider -->
+
+	<!-- Start main content -->
+
+	<main role="main">
+
+		<!-- Start About -->
+		<section id="mu-about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-about-area">
+
+							<div class="mu-about-header">
+								<h2 class="mu-heading-title">ABOUT ME</h2>
+								<span class="mu-header-dot"></span>
+								<span class="mu-header-dot"></span>
+								<p>A <strong> Motion graphics</strong> and a <strong> Video editor</strong> who believes in smart work. I am a native of Nepal and currently living in Wiesbaden, Germany. I have more than 3 years of Video Producing experience. I also have 2 years experience of Video Editing and Motion Graphics Designing. </p>
+							</div>
+
+							<!-- Start About Us Content -->
+							<div class="mu-about-content">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="mu-about-content-left">
+											<img class="mu-profile-img" src="assets/images/profile.jpg" alt="Profile Image">
+										</div>
+									</div>
+									<div class="col-md-6">
+										<div class="mu-about-content-right">
+
+											<!-- Start skills progress bar -->
+
+											<div class="mu-skill-progress-bar">
+												<h3>Skills</h3>
+												<span>Premiere Pro</span>
+												<div class="mu-pro-bar mu-premiere-bar">
+
+												</div>
+												<span>After Effects</span>
+												<div class="mu-pro-bar mu-ae-bar">
+
+												</div>
+												<span>Photoshop</span>
+												<div class="mu-pro-bar mu-photoshop-bar">
+												</div>
+												<span>Illustrator</span>
+												<div class="mu-pro-bar mu-illustrator-bar">
+												</div>
+												<span>InDesign</span>
+												<div class="mu-pro-bar mu-indesign-bar">
+												</div>
+												<span>DaVinci Resolve</span>
+												<div class="mu-pro-bar mu-davinci-bar">
+												</div>
+											</div>
+
+											<!-- Start Social Media 
+						                  	<div class="mu-social-media">
+						                  		<h3>Social</h3>
+						                  		<a href="#"><i class="fa fa-facebook"></i></a>
+						                  		<a href="#"><i class="fa fa-twitter"></i></a>
+						                  		<a href="#"><i class="fa fa-linkedin"></i></a>
+						                  		<a href="#"><i class="fa fa-google-plus"></i></a>
+						                  	</div> -->
+
+
+											<!-- Download Ressume Button -->
+											<a href="http://bit.ly/2H1Ng8u" class="mu-resume-btn">Download My Resume</a>
+
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- End About Us Content -->
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About -->
+
+		<!-- Start Service -->
+		<section id="mu-service">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-service-area">
+
+							<div class="mu-service-header">
+								<h2 class="mu-heading-title">MY SERVICES</h2>
+								<span class="mu-header-dot"></span>
+								<p>As a experienced Video Producer my area of expertise lies in Post-Production, Production and Creative Development.</p>
+							</div>
+
+							<!-- Start Service Content -->
+							<div class="mu-service-content">
+								<div class="row">
+
+									<!-- Service Single Content -->
+									<div class="col-md-4 col-sm-6">
+										<div class="mu-service-content-single">
+											<span class="mu-service-icon-box">
+												<i class="fa fa-leaf"></i>
+											</span>
+											<h4>POST-PRODUCTION</h4>
+											<p>Editing, 2D Graphics Animation, Color Grading and light VFX</p>
+										</div>
+									</div>
+									<!-- / Service Single Content -->
+
+									<!-- Service Single Content -->
+									<div class="col-md-4 col-sm-6">
+										<div class="mu-service-content-single">
+											<span class="mu-service-icon-box">
+												<i class="fa fa-desktop"></i>
+											</span>
+											<h4>PRODUCTION</h4>
+											<p>Camera, Lighting and Photography</p>
+										</div>
+									</div>
+									<!-- / Service Single Content -->
+
+									<!-- Service Single Content -->
+									<div class="col-md-4 col-sm-6">
+										<div class="mu-service-content-single">
+											<span class="mu-service-icon-box">
+												<i class="fa fa-briefcase"></i>
+											</span>
+											<h4>CREATIVE DEVELOPMENT</h4>
+											<p>Concept, Scene Composition</p>
+										</div>
+									</div>
+									
+						
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Service -->
+
+		<!-- Start Portfolio -->
+		<section id="mu-portfolio">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-portfolio-area">
+
+							<div class="mu-portfolio-header">
+								<h2 class="mu-heading-title">MY PORTFOLIO</h2>
+
+								<span class="mu-header-dot"></span>
+							</div>
+						</div>
+						<div class="swiper-container gallery-top">
+							<div class="swiper-wrapper">
+								<div class="swiper-slide">
+									<img src="images/nature1.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature2.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature3.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature4.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature1.jpg" />
+								</div>
+
+							</div>
+							<!-- Add Arrows -->
+							<div class="swiper-button-next swiper-button-white"></div>
+							<div class="swiper-button-prev swiper-button-white"></div>
+						</div>
+
+
+
+						<div class="swiper-container gallery-thumbs">
+							<div class="swiper-wrapper">
+								<div class="swiper-slide">
+									<img src="images/nature1.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature2.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature3.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature4.jpg" />
+								</div>
+								<div class="swiper-slide">
+									<img src="images/nature1.jpg" />
+								</div>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Portfolio -->
+
+
+		<section id="mu-contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mu-contact-area">
+
+							<div class="mu-contact-header">
+								<h2 class="mu-heading-title">CONTACT ME</h2>
+								<span class="mu-header-dot"></span>
+							</div>
+							
+							
+                            
+							<div class="col-md-12">
+								<div class="mu-contact-bottom">
+									<div id="form-messages"></div>
+									<form action="mailer.php" id="ajax-contact" method="post"  class="mu-contact-form">
+
+									    <div class="form-group">
+											<input type="text" class="form-control" placeholder="Name" id="name" name="name" required>
+										</div>
+										<div class="form-group">
+											<input type="email" class="form-control" placeholder="Enter Email" id="email" name="email" required>
+										</div>
+										<div class="form-group">
+											<textarea class="form-control" placeholder="Message" id="message" name="message" required></textarea>
+										</div>
+										<button type="submit" name="submit" id="submit" class="mu-send-msg-btn"><span>SUBMIT</span></button>
+
+									</form>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Contact -->
+
+	</main>
+
+	<!-- End main content -->
+
+
+	<!-- Start footer -->
+	<footer id="mu-footer" role="contentinfo">
+		<div class="container">
+			<div class="mu-footer-area">
+				<p class="mu-copy-right">&copy; Copyright <a rel="nofollow" href="http://onlysndp.me">onlysndp.me</a>. All right reserved.</p>
+			</div>
+		</div>
+
+	</footer>
+	<!-- End footer -->
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+	<script>
+		particlesJS.load('particles-js', 'particles.json',
+			function() {
+				console.log('particles.json loaded...')
+			});
+	</script>
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<!-- Bootstrap -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- Slick slider -->
+	<script type="text/javascript" src="assets/js/slick.min.js"></script>
+	<!-- Filterable Gallery js -->
+	<script type="text/javascript" src="assets/js/jquery.filterizr.min.js"></script>
+	<!-- Gallery Lightbox -->
+	<script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- Ajax contact form  -->
+	<script type="text/javascript" src="assets/js/app.js"></script>
+	<!-- Featured Slider -->
+	<script type="text/javascript" src="assets/js/typed.min.js"></script>
+	<!-- On scroll JS  -->
+	<script src="assets/js/jquery.appear.js"></script>
+	<!-- Progress Bar -->
+	<script type="text/javascript" src="assets/js/jquery.lineProgressbar.js"></script>
+
+
+	<!-- Custom js -->
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+
+
+	<script src="assets/js/app.js"></script>
+	<script src="assets/js/swiper.min.js"></script>
+	<script>
+		var galleryThumbs = new Swiper('.gallery-thumbs', {
+			spaceBetween: 10,
+			slidesPerView: 4,
+			loop: true,
+			freeMode: true,
+			loopedSlides: 5, //looped slides should be the same
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+		});
+		var galleryTop = new Swiper('.gallery-top', {
+			spaceBetween: 10,
+			loop: true,
+			loopedSlides: 5, //looped slides should be the same
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			thumbs: {
+				swiper: galleryThumbs,
+			},
+		});
+	</script>
+</body>
+
+</html>
--- mailer.php
+++ mailer.php
@@ -1,56 +1,35 @@
+
 <?php
-    // My modifications to mailer script from:
-    // http://blog.teamtreehouse.com/create-ajax-contact-form
-    // Added input sanitizing to prevent injection
+    
+    require 'phpmailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+    
+    //$mail->SMTPDebug = 3;
+    //$mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->Port = 587; 
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'phpmailer.onlysndp@gmail.com';                  // SMTP username
+    $mail->Password =  'pHp@Ma!ler(1993)';                             // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                                    
+    $mail->setFrom($_POST['email'],$_POST['name']);
+    $mail->addAddress('sandiip.khanal@gmail.com');     // Add a recipient
+   
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        // $cont_subject = trim($_POST["subject"]);
-        $message = trim($_POST["message"]);
+    $mail->isHTML(true);                                  // Set email format to HTML
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
-
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "sandiip.khanal@gmail.com";
-
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        // $email_content .= "Subject: $cont_subject\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
+    $mail->Subject = 'Interested in Resume';
+    $mail->Body    = 'Message: '.$_POST['message'].'  Email: '.$_POST['email'].'Name: '.$_POST['name'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+   
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } 
+    else {
+        echo 'Thank you for reaching out. We will contact soon';
+   }
+  
 
 ?>
